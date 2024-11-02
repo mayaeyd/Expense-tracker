f6b6e09d2f49c9f583d9c7481d6a6debc6de7935
Maya Eid <mayabaseleid@gmail.com>
2024-11-02 15:52:42 +0200

    Add slide animation to show send money form in transactions

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+document.getElementById("send-money").addEventListener('click',()=>{
+    const item2= document.getElementById('item2');
+    const item3= document.getElementById("item3");
+    if (item3.style.display === 'none' || item3.style.display === '') {
+        item3.style.display = 'flex';
+        setTimeout(() => {
+            item2.style.gridRow=1;
+            item3.style.transform = 'translateY(0)'; 
+        }, 100); 
+    }
+});
