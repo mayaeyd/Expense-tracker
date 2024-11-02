89e8aad1923e2f9098f2da3a2bc676750d58a303
Maya Eid <mayabaseleid@gmail.com>
2024-11-02 19:02:56 +0200

    Ensure dashboard recieves transaction history and display them in dashboard (test)

---
+++ scripts/transactions.js
@@ -0,0 +1,30 @@
+document.getElementById("send-money").addEventListener('click',()=>{
+    const item2= document.getElementById('item2');
+    const item3= document.getElementById("item3");
+    if (item3.style.display === 'none' || item3.style.display === '') {
+        item3.style.display = 'flex';
+        setTimeout(() => {
+            item2.style.gridRow=1;
+            item3.style.transform = 'translateY(0)'; 
+        }, 100); 
+    }
+});
+
+document.getElementById("transactions-form").addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const name = document.getElementById("reciever-name").value;
+    const amount = document.getElementById('transaction-amount').value;
+    const phoneNumber = document.getElementById('reciever-tel').value;
+    const currency = document.getElementById('currency').value;
+
+    const transaction = {
+        id:Date.now().toString(),
+        name:name,
+        amount:amount,
+        tel: phoneNumber,
+        currency: currency
+    };
+
+    addTransaction(transaction);
+});
