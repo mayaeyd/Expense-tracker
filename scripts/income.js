61f138177e37749d6772ba185a4df22f42b572fe
Maya Eid <mayabaseleid@gmail.com>
2024-11-03 12:56:40 +0200

    Refine styling in income page and add table headers

--- scripts/income.js
+++ scripts/income.js
@@ -5,14 +5,12 @@ document.addEventListener('DOMContentLoaded',()=>{
     document.getElementById('balance').innerHTML =balance;
 
     if(balance>2000){
-        document.getElementById('arrow').innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#43AB53" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>'
+        document.getElementById('arrow').innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#189C34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>';
     }else if(balance<2000){
         document.getElementById('arrow').innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#E02C2A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-left"><path d="M17 7 7 17"/><path d="M17 17H7V7"/></svg>'
     }
+
+    document.getElementById('total-income').style.color='#189C34';
+    document.getElementById('expenses').style.color='#E02C2A';
+
 });
-
-
-
-
-{/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg> */}
-{/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-left"><path d="M17 7 7 17"/><path d="M17 17H7V7"/></svg> */}
