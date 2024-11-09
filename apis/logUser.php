55549d27cfea260c4d33fcf79c2b4dfd4ce4dcc4
Maya Eid <mayabaseleid@gmail.com>
2024-11-09 09:13:08 +0200

    Check if password is correct during login

--- apis/logUser.php
+++ apis/logUser.php
@@ -1,20 +1,49 @@
 <?php
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET");
 
-require "connection.php";
+include "connection.php";
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $_POST["username"];
+$password = $_POST["password"];
 
-$stmt = $connection->prepare("INSERT INTO users (name,password) VALUES (?,?)");
 
-if (! $stmt) {
-    die("Statement preparation failed: " . $connection->error);
+$query = $connection->prepare("SELECT * FROM users WHERE name = ?");
+$query->bind_param("s", $username);
+$query->execute();
+
+$result = $query->get_result();
+$user = $result->fetch_assoc();
+
+if ($user) {
+
+    echo "Stored hash: " . $user["password"];
+    echo "Submitted password: " . $password;
+
+    $check = password_verify($password, $user["password"]);
+
+    if($check){
+        //send a json response with the user id
+        echo json_encode([
+            "status"=>"Login Successful",
+            "user"=>$user["id"],
+        ]);
+        header("Location: ../dashboard.html");
+    }else{
+        echo json_encode([
+            "status" => "Password Incorrect",
+        ]);
+    }
+    //exit();
+}else{
+    echo json_encode([
+        "status" => "Invalid Credentials",
+        "message" => "Could not create records",
+    ]);
 }
 
-$stmt->bind_param("ss",$username, $password);
-$stmt->execute();
-echo "Insert Successful.";
-$stmt->close();
+$query->close();
 $connection->close();
 
 ?>
