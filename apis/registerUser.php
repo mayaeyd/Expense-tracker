77078349ef32598df62a917419b8ae6fc8d6291a
Maya Eid <mayabaseleid@gmail.com>
2024-11-09 08:34:22 +0200

    Check for duplicates of username in registration

--- apis/registerUser.php
+++ apis/registerUser.php
@@ -1,29 +1,36 @@
 <?php
 
-require "connection.php";
+include "connection.php";
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$hashed = password_hash($password, PASSWORD_DEFAULT);
+//$hashed = password_hash($password, PASSWORD_DEFAULT);
 
-$query = $connection->prepare("INSERT INTO users (name,password) VALUES (?,?)");
+$duplicate =  $connection->prepare("SELECT * FROM users WHERE name = ?");
+$duplicate->bind_param("s",$username);
+$duplicate->execute();
+if($duplicate->get_result()->num_rows>0){
+    echo "Username has already been taken";
+}else{
+    $query = $connection->prepare("INSERT INTO users (name,password) VALUES (?,?)");
 
-if (! $query) {
-    die("Statement preparation failed: " . $connection->error);
+    if (! $query) {
+        die("Statement preparation failed: " . $connection->error);
+    }
+
+    $query->bind_param("ss",$username, $password);
+    $query->execute();
+
+    if ($query->affected_rows !=0) {
+        header("Location: ../dashboard.html");
+        exit();
+    } else {
+        echo "Error: " . $query->error;
+    }
+    $query->close();
 }
 
-$query->bind_param("ss",$username, $hashed);
-$query->execute();
-
-if ($query->affected_rows !=0) {
-    header("Location: ../dashboard.html");
-    exit();
-} else {
-    echo "Error: " . $query->error;
-}
-
-$query->close();
 $connection->close();
 
 ?>
